338d1342ed7f24b672181331202e75f0b673eefb
luk156 <matteo@luccalug.it>
2013-06-11 14:14:09 +0200

    fix return sac

--- unisci.py
+++ unisci.py
@@ -88,7 +88,7 @@ def generate_sac(start, stop, data_folder, destination_folder="./", file_name='d
 	print "writing ", file_name
 	os.chdir(destination_folder)
 	speed_trace.WriteSacBinary(file_name)
-	return file_name
+	return destination_folder+str(file_name)
 
 def generate_raw_sac(speed100, start, destination_folder="./", file_name='default' ):
 	speed_trace = sac.SacIO() # inizializzo un oggetto sac
@@ -100,7 +100,7 @@ def generate_raw_sac(speed100, start, destination_folder="./", file_name='defaul
 	os.chdir(destination_folder)
 	speed_trace.WriteSacBinary(file_name)
 	print "creato il file:", file_name
-	return file_name
+	return destination_folder+str(file_name)
 
 
 
