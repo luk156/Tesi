2fec20444effed4e422e51927094f1ffd9297444
luk156 <matteo@luccalug.it>
2013-08-26 09:56:27 +0200

    26 agosto

--- sismo.py
+++ sismo.py
@@ -9,11 +9,68 @@ def read_sismo_stream(data, data_folder, starttime=None, endtime=None ):
 	file_path = data_folder+data.strftime('%Y.%m.%d')+"-00.00.00.A295.*.SAC.resamp"
 	print 'open', file_path
 	stream = ob.read(file_path, starttime=starttime, endtime=endtime)
-	for i in range(1,3):
-		stream[i].stats.paz = ob.core.AttribDict({
-			'poles': [(-0.037+0.037j), (-0.037-0.037j) ],
-			'sensitivity': 1500.0,
-			'zeros': [0j, 0j],
-			'gain': 1.0})
 	return stream
 
+sts2 = {
+	'gain': 1500.0,
+	'poles': [ ],
+	'sensitivity': 1/1.5896e-6,
+	'zeros': [] }
+
+def read_sismo_stream2(data_folder, starttime, endtime ):
+	k=starttime
+	file_path = data_folder+starttime.strftime('%Y.%m.%d')+"-00.00.00.A295.*.SAC.resamp"
+	print 'open', file_path
+	stream = ob.read(file_path, starttime=starttime, endtime=endtime)
+	for c in stream:
+		c.data=c.data*1.5896e-6/1500.0
+	stream.detrend('simple')
+	#stream.detrend('linear')
+	print stream
+	k+=dt.timedelta(days = 1)
+	while k<endtime+dt.timedelta(days = 1):
+		file_path = data_folder+k.strftime('%Y.%m.%d')+"-00.00.00.A295.*.SAC.resamp"
+		print 'open', file_path
+		s = ob.read(file_path, starttime=starttime, endtime=endtime)
+		for c in s:
+			c.data=c.data*1.5896e-6/1500.0
+		s.detrend('simple')
+		#s.detrend('linear')
+		stream += s
+		stream.merge(method=0, interpolation_samples=0, fill_value=0)
+		print stream[0].data.shape[0]
+		print stream
+		k+=dt.timedelta(days = 1)
+	#stream.detrend(type='simple')
+	return stream
+
+def read_sismo_stream3(data_folder, starttime, endtime ):
+	k=starttime
+	file_path = data_folder+starttime.strftime('%Y.%m.%d')+"-00.00.00.A295.*.SAC.resamp"
+	print 'open', file_path
+	stream = ob.read(file_path, starttime=starttime, endtime=endtime)
+	for c in stream:
+		c.data=c.data*1.5896e-6/1500.0
+		c.data[0]=c.data[1]
+		c.data[-1]=c.data[-2]
+	stream.detrend('simple')
+	#stream.detrend('linear')
+	print stream
+	k+=dt.timedelta(days = 1)
+	while k<endtime+dt.timedelta(days = 1):
+		file_path = data_folder+k.strftime('%Y.%m.%d')+"-00.00.00.A295.*.SAC.resamp"
+		print 'open', file_path
+		s = ob.read(file_path, starttime=starttime, endtime=endtime)
+		for c in s:
+			c.data=c.data*1.5896e-6/1500.0
+			c.data[0]=c.data[1]
+			c.data[-1]=c.data[-2]
+		s.detrend('simple')
+		#s.detrend('linear')
+		stream += s
+		stream.merge()
+		print stream[0].data.shape[0]
+		print stream
+		k+=dt.timedelta(days = 1)
+	#stream.detrend(type='simple')
+	return stream
--- unisci.py
+++ unisci.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python
-
 import os
 import obspy as ob
 import numpy as np
@@ -21,17 +19,19 @@ def read_gyro_file(ora, data_folder, header_lenght =202, channel_array = np.arra
 			day = ora.day,
 			hour = ora.hour,
 			)
-	file_path = data_folder+str(ora.year)+"/"+str(ora.month)+"/"+str(ora.day)+"/data"+str(ora.hour)+".dat" #ricostruisco il path completo del file
-	print 'open', file_path
-	raw_data = np.fromfile(file_path, dtype=np.float32, count=-1) # carico il contenuto del file all' interno di un vettore
-	N_seconds = raw_data.shape[0]/(channel_array.sum()+header_lenght) # calcolo il numero di secondi campionati presenti nel file
-	print "il file contiene ", N_seconds, " secondi" 
 	header = np.zeros([3600, header_lenght], dtype = np.float32) # inizializzo il vettore header
 	data=[]
 	for k in range(channel_array.shape[0]):
-		data.append(np.zeros(channel_array[k]*3600))
+		data.append(np.zeros(channel_array[k]*3600)) #inizializzo la lista di vettori per i dati
 	data_lenght = header_lenght + channel_array.sum() # calcolo la lunghezza di un secondo di dati
-	start_trace = dt.datetime(
+	file_path = data_folder+str(ora.year)+"/"+str(ora.month)+"/"+str(ora.day)+"/data"+str(ora.hour)+".dat" #ricostruisco il path completo del file
+	print 'open', file_path
+	try:
+		raw_data = np.fromfile(file_path, dtype=np.float32, count=-1) # carico il contenuto del file all' interno di un vettore
+		N_seconds = raw_data.shape[0]/(channel_array.sum()+header_lenght) # calcolo il numero di secondi campionati presenti nel file
+		print "il file contiene ", N_seconds, " secondi" 
+		print "dalle ore: ",  ora - dt.timedelta(seconds=33), "alle: " ,ora - dt.timedelta(seconds=33) + dt.timedelta(hours=1)
+		start_trace = dt.datetime(
 			year = raw_data[10],
 			month = raw_data[9],
 			day = raw_data[8],
@@ -39,17 +39,23 @@ def read_gyro_file(ora, data_folder, header_lenght =202, channel_array = np.arra
 			minute = raw_data[6],
 			second = raw_data[5],
 			)
-	if start_trace>ora:
-		print "il file contiene l'inizio della traccia pertanto potrebbero esserci degli artefatti"
-	for j in range(N_seconds):
-		sample = raw_data[ j * data_lenght : j * data_lenght + header_lenght ][47] # secondo che sto campionando
-		header[sample][:] = raw_data[ j * data_lenght : j * data_lenght + header_lenght ]
-		start_data = j * data_lenght + header_lenght
-		i = 0
-		for k in range(4):
-			data[k][(sample) * channel_array[k] : (sample+1) * channel_array[k] ] = raw_data[ start_data + i :  start_data + i + channel_array[k]]
-			i+=channel_array[k]
-	return header, data, ora - dt.timedelta(seconds=33)
+		if start_trace>ora:
+			print "il file contiene l'inizio della traccia pertanto potrebbero esserci degli artefatti"
+		for j in range(N_seconds):
+			sample = raw_data[ j * data_lenght : j * data_lenght + header_lenght ][47] # secondo che sto campionando
+			header[sample][:] = raw_data[ j * data_lenght : j * data_lenght + header_lenght ]
+			start_data = j * data_lenght + header_lenght
+			i = 0
+			for k in range(4):
+				data[k][(sample) * channel_array[k] : (sample+1) * channel_array[k] ] = raw_data[ start_data + i :  start_data + i + channel_array[k]]
+				i+=channel_array[k]
+		found=True
+		del raw_data
+	except(IOError, OSError):
+		print "file non trovato"
+		found=False
+	start_data = ora - dt.timedelta(seconds=33)
+	return header, data, start_data, found
 
 
 def decimate_gyro_data(sagnac, low = 110, high = 200, corners = 1, zerophase = True, cal = 632.8e-9/1.35/2.0/np.pi ):
@@ -61,30 +67,44 @@ def decimate_gyro_data(sagnac, low = 110, high = 200, corners = 1, zerophase = T
 	speed100 = scipy.signal.decimate( speed, 50 )
 	return speed100 # ritorno la velocita' a 100 Hertz
 
-def generate_sac(start, stop, data_folder, destination_folder="./", file_name='default' ,extra_points=1000, channel_array = np.array([5000,5000,5000,5000]), sagnac_index = 1):
+def generate_sac(start, stop, data_folder, destination_folder="./", file_name='default' , extra_points=0, channel_array = np.array([5000,5000,5000,5000]), sagnac_index = 0):
 	speed_trace = sac.SacIO() # inizializzo un oggetto sac
-	ora = start - dt.timedelta(seconds=33+extra_points/5000) # ora l'indice dei tempi, lo sposto indietro di 33 secondi per via dell'offset e del buffer
-	header,data,start_data = read_gyro_file(ora, data_folder, channel_array = channel_array )
+	ora = start - dt.timedelta(extra_points/5000) + dt.timedelta(seconds=33) #ora rappresente l'indice dei tempi, lo sposto indietro per creare un buffer
+	header,data,start_data, found = read_gyro_file(ora , data_folder, channel_array = channel_array )
 	diff_data_seconds = (start-start_data).seconds # calcolo i secondi da rimuovere
 	ora = ora + dt.timedelta(hours = 1) # sposto l'indice dei tempi avanti di un' ora
-	speed100 = decimate_gyro_data( data[sagnac_index] ) # eleboro il sagnac ed ottengo la velocita' a 100 Hz
+	if found: #se il file dell'ora esiste
+		speed100 = decimate_gyro_data( data[sagnac_index] ) # elaboro il sagnac ed ottengo la velocita' a 100 Hz
+		if extra_points>0:
+			data_buffer = data[sagnac_index][-extra_points:] # salvo in un buffer gli ultimi extra_points
+		else:
+			data_buffer = np.zeros(0)
+	else: # se il file non esiste il vettore data e' composto da soli 0
+		speed100 = scipy.signal.decimate( data[sagnac_index], 50 ) # se non e' stato trovato il file decimo e basta
+		data_buffer = np.zeros(extra_points) # creo un buffer di zeri
 	speed100 = np.delete( speed100, range(0, diff_data_seconds*100) ) # rimuovi punti fino a start
-	data_buffer = data[sagnac_index][-extra_points:] # salvo in un buffer gli ultimi extra_points
-	while stop > start_data + dt.timedelta(hours = 1):
-		header1,data1,start_data = read_gyro_file(ora, data_folder,  channel_array = channel_array )
+	while stop > start_data + dt.timedelta(hours = 1): # ripeto l'operazione fino a stop
+		header1,data1,start_data,found = read_gyro_file(ora, data_folder,  channel_array = channel_array )
 		data1 = np.append( data_buffer, data1[sagnac_index])
 		ora = ora + dt.timedelta(hours = 1)
-		speed100_1 = decimate_gyro_data(data1 )
-		speed100_1 = np.delete(speed100_1, range(0, extra_points/50) )
+		if found:
+			speed100_1 = decimate_gyro_data( data1 )
+			if extra_points>0:
+				data_buffer = data1[-extra_points:]
+			else:
+				data_buffer = np.zeros(0)
+			speed100_1 = np.delete(speed100_1, range(0, extra_points/50) )
+		else:
+			speed100_1 = scipy.signal.decimate( data1, 50 ) # se non e' stato trovato il file decimo e basta
+			data_buffer = np.zeros(0)
 		speed100 = np.append(speed100, speed100_1)
-		data_buffer = data1[-extra_points:]
-	diff_data_seconds = (start_data + dt.timedelta(hours = 1, seconds=33) - stop).seconds
+	diff_data_seconds = (start_data + dt.timedelta(hours = 1) - stop).seconds
 	speed100 = np.delete(speed100, range(speed100.shape[0] - diff_data_seconds*100, speed100.shape[0]) ) # rimuovi punti prima di stop
 	speed_trace.fromarray(speed100, starttime=ob.UTCDateTime(start)) # genero una traccia dall'array delle velocita'
 	speed_trace.SetHvalue('kinst', 'G-Laser Pisa')
 	speed_trace.SetHvalue('delta', 0.01)
 	if file_name=="default":
-		file_name="G-Laser-"+str(start.year)+"_"+str(start.month)+"_"+str(start.day)+"-"+str(start.hour)+":"+str(start.minute)+"_"+str(stop.hour)+":"+str(stop.minute)+".SAC"
+		file_name="G-Laser-"+str(start.year)+"_"+str(start.month)+"_"+str(start.day)+"-"+str(start.hour)+":"+str(start.minute)+"_"+str(stop.month)+"_"+str(stop.day)+"-"+str(stop.hour)+":"+str(stop.minute)+".SAC"
 	print "writing ", file_name
 	os.chdir(destination_folder)
 	speed_trace.WriteSacBinary(file_name)
